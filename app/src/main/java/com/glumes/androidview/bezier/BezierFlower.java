e3f76f3d8ab9dc56a437a5e2587446285b349c6f
glumes <zhaoying9402@gmail.com>
2017-02-12 15:41:19 +0800

    start bezier flower

---
+++ app/src/main/java/com/glumes/androidview/bezier/BezierFlower.java
@@ -0,0 +1,29 @@
+package com.glumes.androidview.bezier;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by zhaoying on 2017/2/12.
+ */
+
+public class BezierFlower extends RelativeLayout {
+
+
+
+    public BezierFlower(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    public BezierFlower(Context context) {
+        super(context);
+    }
+
+    public BezierFlower(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
